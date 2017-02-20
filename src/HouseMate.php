c3a46485b23ec135d430b8685dfe222dfe32dcad
Keith Evans and Katie Reed <keith@>
2017-02-20 14:06:03 -0800

    create house_mate class and passing test conditions

--- src/HouseMate.php
+++ src/HouseMate.php
@@ -1,13 +1,23 @@
 <?php
 
-    class Location
+    class HouseMate
     {
         private $name;
+        private $age;
+        private $profession;
+        private $gender;
+        private $week_joined;
+        private $week_left;
         private $id;
 
-        function __construct($name, $id = null)
+        function __construct($name, $age, $profession, $gender, $week_joined, $week_left, $id = null)
         {
             $this->name = $name;
+            $this->age = $age;
+            $this->profession = $profession;
+            $this->gender = $gender;
+            $this->week_joined = $week_joined;
+            $this->week_left = $week_left;
             $this->id = $id;
         }
 
@@ -21,6 +31,56 @@
             return $this->name;
         }
 
+        function setAge($new_age)
+        {
+            $this->age = $new_age;
+        }
+
+        function getAge()
+        {
+            return $this->age;
+        }
+
+        function setProfession($new_profession)
+        {
+            $this->profession = $new_profession;
+        }
+
+        function getProfession()
+        {
+            return $this->profession;
+        }
+
+        function setGender($new_gender)
+        {
+            $this->gender = $new_gender;
+        }
+
+        function getGender()
+        {
+            return $this->gender;
+        }
+
+        function setWeekJoined($new_week_joined)
+        {
+            $this->week_joined = $new_week_joined;
+        }
+
+        function getWeekJoined()
+        {
+            return $this->week_joined;
+        }
+
+        function setWeekLeft($new_week_left)
+        {
+            $this->week_left = $new_week_left;
+        }
+
+        function getWeekLeft()
+        {
+            return $this->week_left;
+        }
+
         function getId()
         {
             return $this->id;
@@ -28,32 +88,38 @@
 
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO locations (name) VALUES ('{$this->getName()}')");
+            $GLOBALS['DB']->exec(
+            "INSERT INTO house_mates (name, age, profession, gender, week_joined, week_left) VALUES ('{$this->getName()}', {$this->getAge()}, '{$this->getProfession()}', '{$this->getGender()}', '{$this->getWeekJoined()}', '{$this->getWeekLeft()}');");
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
         {
-            $returned_locations = $GLOBALS['DB']->query("SELECT * FROM locations");
-            $locations_array = [];
-            foreach ($returned_locations as $locations) {
-                $name = $locations['name'];
-                $id = $locations['id'];
-                $new_location = new Location($name, $id);
-                array_push($locations_array, $new_location);
+            $returned_house_mates = $GLOBALS['DB']->query("SELECT * FROM house_mates");
+            $house_mates_array = [];
+            foreach ($returned_house_mates as $house_mates) {
+                $name = $house_mates['name'];
+                $age = $house_mates['age'];
+                $profession = $house_mates['profession'];
+                $gender = $house_mates['gender'];
+                $week_joined = $house_mates['week_joined'];
+                $week_left = $house_mates['week_left'];
+                $id = $house_mates['id'];
+                $new_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left, $id);
+                array_push($house_mates_array, $new_HouseMate);
             }
-            return $locations_array;
+            return $house_mates_array;
         }
 
         static function deleteAll()
         {
-            $GLOBALS["DB"]->exec("DELETE FROM locations");
+            $GLOBALS["DB"]->exec("DELETE FROM house_mates");
         }
 
         static function find($search_id)
         {
             $return_location = null;
-            $results = Location::getAll();
+            $results = HouseMate::getAll();
             foreach ($results as $result) {
                 $location_id = $result->getId();
                 if ($location_id == $search_id) {
---
+++ tests/HouseMateTest.php
@@ -0,0 +1,253 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/HouseMate.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=terrace_house_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class HouseMateTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown() {
+            HouseMate::deleteAll();
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getAge()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getAge();
+
+            //Assert
+            $this->assertEquals($age, $result);
+        }
+
+        function test_getProfession()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getProfession();
+
+            //Assert
+            $this->assertEquals($profession, $result);
+        }
+
+        function test_getGender()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getGender();
+
+            //Assert
+            $this->assertEquals($gender, $result);
+        }
+
+        function test_getWeekJoined()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getWeekJoined();
+
+            //Assert
+            $this->assertEquals($week_joined, $result);
+        }
+
+        function test_getWeekLeft()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+
+            //Act
+            $result = $test_HouseMate->getWeekLeft();
+
+            //Assert
+            $this->assertEquals($week_left, $result);
+        }
+
+        function test_getID()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $id = 1;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left, $id);
+
+            //Act
+            $result = $test_HouseMate->getID();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate->save();
+
+            //Act
+            $result = HouseMate::getAll();
+
+            //Assert
+            $this->assertEquals($test_HouseMate, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate->save();
+            $name2 = "Yuriko";
+            $age2 = 23;
+            $profession2 = 'medical student';
+            $gender2 = 'female';
+            $week_joined2 = 1;
+            $week_left2 = 13;
+            $test_HouseMate2 = new HouseMate($name2, $age2, $profession2, $gender2, $week_joined2, $week_left2);
+            $test_HouseMate2->save();
+
+            //Act
+            $result = HouseMate::getAll();
+
+            //Assert
+            $this->assertEquals([$test_HouseMate, $test_HouseMate2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate->save();
+            $name = "Yuriko";
+            $age = 23;
+            $profession = 'medical student';
+            $gender = 'female';
+            $week_joined = 1;
+            $week_left = 13;
+            $test_HouseMate2 = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate2->save();
+
+            //Act
+            HouseMate::deleteAll();
+            $result = HouseMate::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $name = "Makoto";
+            $age = 22;
+            $profession = 'baseball player';
+            $gender = 'male';
+            $week_joined = 1;
+            $week_left = 11;
+            $test_HouseMate = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate->save();
+            $name = "Yuriko";
+            $age = 23;
+            $profession = 'medical student';
+            $gender = 'female';
+            $week_joined = 1;
+            $week_left = 13;
+            $test_HouseMate2 = new HouseMate($name, $age, $profession, $gender, $week_joined, $week_left);
+            $test_HouseMate2->save();
+
+            //Act
+            $result = HouseMate::find($test_HouseMate->getId());
+
+            //Assert
+            $this->assertEquals($test_HouseMate, $result);
+        }
+
+    }
+
+?>
